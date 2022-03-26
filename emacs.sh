e0c2751eb95abfde558abcc14ed3251d5e41dcf3
Marie Katrine Ekeberg <mke@themkat.net>
2022-03-26 16:13:05 +0100

    Simple script for automating creation of dir locals settings file for Emacs when doing C programming. First functions are to create GameCube devkitpro settings

---
+++ emacs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script with various utilities for GNU Emacs
+# TODO: dir-locals for devkitpro, Unreal Engine C++ stuff etc.
+# TODO any way I could make more general functions to help make all kinds of dir-locals files? Different modes etc.? Think about it :) 
+# TODO: maybe also the possibility to modify a dir-locals file if present?
+
+function __emacs_create_dir_locals_c_config() {
+	HEADERS=$1
+	HEADERS_QUOTED=$(echo $1 | tr ' ' '\n' | sed -E 's/(.*)/\"\1\"/')
+	CLANG_ARGS=$(echo "$HEADERS" | tr ' ' '\n' | sed -E 's/(.*)/\"-I\1\"/')
+	
+	echo "((c-mode . ((company-clang-arguments . ($CLANG_ARGS))\n\
+	        (company-c-headers-path-system . ($HEADERS_QUOTED)))))" > .dir-locals.el
+}
+
+# create a dir locals file for gamecube with devkitpro clang and header config
+function emacs_create_dir_locals_gamecube() {
+	HEADERS="$DEVKITPRO/libogc/include/"
+	__emacs_create_dir_locals_c_config "$HEADERS"
+}
+
+function emacs_create_dir_locals_gamecube_portlibs() {
+	HEADERS="$DEVKITPRO/libogc/include/ $DEVKITPRO/portlibs/gamecube/include/"
+	__emacs_create_dir_locals_c_config "$HEADERS"
+}
